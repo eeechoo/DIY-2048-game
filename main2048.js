638135caab1d16e5152851e5dea01eb37c069766
eeechoo <caiminghui369@gmail.com>
2018-04-17 12:25:29 +0800

    游戏界面初始化
    1. 初始化所有的number-cell，都将其宽高设置成0
    2. 随机生成2个位置上的数字，将其通过动画显示出来

--- main2048.js
+++ main2048.js
@@ -9,6 +9,8 @@ function newgame() {
     //初始化棋盘格
     init()
     //在随机两个格子生成数字
+    generateOneNumber()
+    generateOneNumber()
 }
 
 function init() {
@@ -19,4 +21,64 @@ function init() {
             gridCell.css('left', getPosLeft(i, j))
         }
     }
+
+    for (var i = 0; i < 4; i++) {
+        board[i] = new Array()
+        for (var j = 0; j < 4; j++) {
+            board[i][j] = 0
+        }
+    }
+    updateBoardView()
+}
+
+function updateBoardView() {
+    $('.number-cell').remove()
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            $('#grid-container').append(`<div class="number-cell" id="number-cell-${i}-${j}"></div>`)
+            var theNumberCell = $(`#number-cell-${i}-${j}`)
+
+            if (board[i][j] === 0) {
+                theNumberCell.css('width', '0px')
+                theNumberCell.css('height', '0px')
+                theNumberCell.css('top', getPosTop(i, j) + 50)
+                theNumberCell.css('left', getPosLeft(i, j) + 50)
+                theNumberCell.css('background-color', getNumberBackgroundColor(board[i][j]))
+                theNumberCell.css('color', getNumberColor(board[i][j]))
+            } else {
+                theNumberCell.css('width', '100px')
+                theNumberCell.css('height', '100px')
+                theNumberCell.css('top', getPosTop(i, j))
+                theNumberCell.css('left', getPosLeft(i, j))
+                theNumberCell.css('background-color', getNumberBackgroundColor(board[i][j]))
+                theNumberCell.css('color', getNumberColor(board[i][j]))
+            }
+        }
+    }
+}
+
+function generateOneNumber() {
+    if(nospace(board)) {
+        return false
+    }
+
+    //随机一个位置
+    var randx = parseInt(Math.floor(Math.random() * 4))
+    var randy = parseInt(Math.floor(Math.random() * 4))
+
+    while (true) {
+        if (board[randx][randy] === 0) {
+            break
+        }
+        var randx = parseInt(Math.floor(Math.random() * 4))
+        var randy = parseInt(Math.floor(Math.random() * 4))
+    }
+
+    //随机一个数字
+    var randNumber = Math.random() < 0.5 ? 2 : 4
+
+    //在随机位置显示随机数字
+    board[randx][randy] = randNumber
+    showNumberWithAnimation(randx, randy, randNumber)
+    return true
 }
